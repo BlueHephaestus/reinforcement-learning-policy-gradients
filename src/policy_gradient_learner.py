ca6b7fb826fa023e2d845408d3d16d8032b07508
DarkElement75 <blakeedwards777@gmail.com>
2018-04-08 20:09:04 -0600

    Final commit for now; Couldn't get policy gradients to work very well on the two problems I tested after trying various parameters, so i'm moving on to other methods.

--- src/policy_gradient_learner.py
+++ src/policy_gradient_learner.py
@@ -14,6 +14,8 @@ from explore_exploit_policies import *
 import advantage_functions
 from advantage_functions import *
 
+import matplotlib.pyplot as plt
+
 class PolicyGradientModel(object):
     def __init__(self, env_name, epochs=100, mb_n=20, timesteps=200, epsilon=0.5, learning_rate=1.0, discount_factor=1.0):
 
@@ -23,12 +25,12 @@ class PolicyGradientModel(object):
         self.timesteps = timesteps
 
         self.epsilon = epsilon
-        #self.learning_rate = learning_rate
+        self.learning_rate = learning_rate
         self.discount_factor = discount_factor
 
         #So that these will decay to .0001 by the final epoch
-        self.epsilon_decay_rate = -9.0/self.epochs
-        self.learning_rate_decay_rate = -9.0/self.epochs
+        self.epsilon_decay_rate = -6.0/self.epochs
+        self.learning_rate_decay_rate = -6.0/self.epochs
 
         #Initialize our environment 
         print("Initializing Environment...")
@@ -46,7 +48,7 @@ class PolicyGradientModel(object):
         self.render_intermittent = False
 
         #For stats
-        self.costs = np.zeros(self.epochs)
+        self.costs = np.zeros((self.epochs, 2))
         self.avg_rewards = np.zeros((self.epochs))
         self.avg_timesteps = np.zeros((self.epochs))
 
@@ -62,38 +64,43 @@ class PolicyGradientModel(object):
         #Define and Initialize our model
         print("Initializing Model...")
 
-        loss = "categorical_crossentropy"
-        optimizer = Adam(1e-4)
+        #loss = "categorical_crossentropy"
+        #optimizer = Adam(1e-4)
+        optimizer = SGD(lr=self.learning_rate, decay=-self.learning_rate_decay_rate)
 
         self.model = Sequential()
         self.model.add(Dense(8, activation="sigmoid", input_shape=(self.input_n,)))
+        self.model.add(Dense(8, activation="sigmoid"))
+        self.model.add(Dense(8, activation="sigmoid"))
+        self.model.add(Dense(8, activation="sigmoid"))
+        self.model.add(Dense(8, activation="sigmoid"))
         self.model.add(Dense(8, activation="sigmoid"))
         self.model.add(Dense(self.output_n, activation="softmax"))
         self.model.compile(loss=self.pgrad_loss, optimizer=optimizer, metrics=["accuracy"])
 
     def pgrad_loss(self, predictions, actions):
-        return K.mean(K.categorical_crossentropy(predictions, actions) * self.advantages)
+        return K.mean(K.sum(K.log(predictions)*actions, axis=1) * self.advantages)
 
     def train(self):
 
         print("Training Model on Environment...")
-        for epoch in tqdm(range(self.epochs)):
+        for epoch in range(self.epochs):
             #Decay our epsilon value and learning rate by their decay rates
             self.epsilon *= np.exp(self.epsilon_decay_rate)
             #self.learning_rate *= np.exp(learning_rate_decay_rate)
 
-            for mb in tqdm(range(self.mb_n)):
+            for mb in range(self.mb_n):
                 #TODO: Make stepping parallel because otherwise we literally predict at every step
                 
                 #Reset game environment
                 state = self.env.reset()
 
-                for timestep in tqdm(range(self.timesteps)):
+                for timestep in range(self.timesteps):
                     if self.render:
                         self.env.render()
                     else:
                         if self.render_intermittent:
-                            if epoch % 10 == 0 and mb == 0:
+                            if epoch % 100 == 0 and mb == 0:
                                 self.env.render()
 
                     #Get prediction vector from model and choose action index using our explore exploit policy
@@ -127,9 +134,23 @@ class PolicyGradientModel(object):
             cost = self.model.train_on_batch(self.states, self.actions)
 
             #Save performance stats for this epoch
-            self.costs[epoch] = np.nan_to_num(cost[0])
+            self.costs[epoch] = np.nan_to_num(cost)
             self.avg_rewards[epoch] /= self.mb_n#Compute average from the sum
             self.avg_timesteps[epoch] /= self.mb_n#Compute average from the sum
 
-pgm = PolicyGradientModel("Acrobot-v1", epochs=100, mb_n=20, timesteps=200, epsilon=1.0, learning_rate=3.0)
+            sys.stdout.write("\rEpoch: {}, Costs: {}, Avg. Rewards: {}, Avg Timesteps: {}, Epsilon: {}, Learning Rate: {}".format(epoch+1, self.costs[epoch], self.avg_rewards[epoch], self.avg_timesteps[epoch], self.epsilon, self.learning_rate))
+            sys.stdout.flush()
+
+        plt.subplot(1, 2, 1)
+        plt.plot(self.avg_rewards)
+        plt.subplot(1, 2, 2)
+        plt.plot(self.avg_timesteps)
+        plt.show()
+
+
+            
+
+#pgm = PolicyGradientModel("Acrobot-v1", epochs=100, mb_n=20, timesteps=200, epsilon=1.0, learning_rate=3.0)
+pgm = PolicyGradientModel("CartPole-v0", epochs=4000, mb_n=10, timesteps=200, epsilon=1.0, learning_rate=1.0, discount_factor=0.95)
 pgm.train()
+print("")
